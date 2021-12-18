4ac133d5808822df6e5c1c340b50f202db5a00f9
radsutton <radsutton@hotmail.co.uk>
2021-12-18 16:49:40 +0100

    added max uploads and timeout behaviour plus improved readme for windows users

--- README.md
+++ README.md
@@ -9,13 +9,27 @@ Linux/Unix
 5. login and enter steam email key
 6. congrats all your keys should be redeemed!
 
-Windows
+Windows - venv
  
  1. add keys from email into keys.txt 
- 2. Install python3.9 from windows store
- 3. (powershell)  Set-ExecutionPolicy RemoteSigned  
- 4. (powershell) 'venv_windows/bin/Activate.ps1'
- 5. run 'python steamkey-redeem.py'
- 6. should read all keys in back to you
- 7. login and enter steam email key
- 8. congrats all your keys should be redeemed!
+ 2. Install python3.9 from microsoft store (if you don't have it already)
+ 3. (admin powershell)  Set-ExecutionPolicy RemoteSigned
+ 4. Change the path in venv_windows\pyvenv.cfg to point to your installed python
+ 5. (powershell) 'venv_windows/Scripts/Activate.ps1'
+ 6. within the venv, run 'python steamkey-redeem.py'
+ 7. should read all keys in back to you
+ 8. login and enter steam email key
+ 9. congrats all your keys should be redeemed!
+
+Windows - without venv
+ 
+ 1. add keys from email into keys.txt 
+ 2. Install python3.9 from microsoft store (if you don't have it already)
+ 3. ~~(powershell) python -m venv~~
+ 4. ~~(powershell) 'venv/bin/Activate.ps1'~~
+ 5. python -m pip install -r requirements.txt
+ 6. python -m pip install steam google
+ 7. within the venv, run 'python steamkey-redeem.py'
+ 8. should read all keys in back to you
+ 9. login and enter steam email key
+ 10. congrats all your keys should be redeemed!
--- steamkey-redeem.py
+++ steamkey-redeem.py
@@ -1,7 +1,11 @@
 # Redeeming Steam keys.
 import steam.client
 import re
+import time
 
+STEAM_MAX_ALLOWED_REDEMPTIONS = 50
+STEAM_COOLDOWN_MINUTES = 60
+STEAM_INCORRECT_REDEMPTION_PENALTY = 2
 
 def read_keys(fn='keys.txt'):
     keys = []
@@ -9,8 +13,8 @@ def read_keys(fn='keys.txt'):
         lines = f.readlines()
 
     for i, line in enumerate(lines):
-        chcek = get_key(line)
-        if chcek != []:
+        check = get_key(line)
+        if check:
             name = lines[i-1]
             key = line.split(' ')[2]
             keys.append(key)
@@ -21,10 +25,19 @@ def get_key(line):
     key = re.findall(r'(\w{5}-){2}\w{5}', line)
     return key
 
+def check_attempts(attempt_count):
+    if attempt_count > STEAM_MAX_ALLOWED_REDEMPTIONS:
+        print("Maximum amount of redemptions reached.")
+        print("Waiting " + str(STEAM_COOLDOWN_MINUTES) + " minutes.")
+        print("This program can be safely stopped and restarted after the allotted time.")
+        time.sleep(60*STEAM_COOLDOWN_MINUTES)
+        return 0
+    return attempt_count + 1
 
 def redeem(keys):
     # Init Steam client.
     steam_client: steam.client.SteamClient = steam.client.SteamClient()
+    attempt_count = 0
 
     print("Steam Login \n")
     # Login to Steam client.
@@ -34,20 +47,23 @@ def redeem(keys):
         # Print current key.
         print("Key:", key)
 
+        attempt_count = check_attempts(attempt_count)
         # Try to redeem key.
-        eresult, result_details, receipt_info = steam_client.register_product_key(key)
+        result, result_details, receipt_info = steam_client.register_product_key(key)
 
         # Has game already?
-        has_already = eresult == steam.client.EResult.Fail and result_details == 9
+        has_already = result == steam.client.EResult.Fail and result_details == 9
 
         # If did not has already and did not success.
-        if not has_already and eresult != steam.client.EResult.OK:
+        if not has_already and result != steam.client.EResult.OK:
             # Raise exception.
-            print("Code redeem failed!", eresult, result_details, receipt_info)
-            #raise Exception("Code redeem failed!", eresult, result_details, receipt_info)
+            print("Code redeem failed!", result, result_details, receipt_info)
+            attempt_count = attempt_count + STEAM_INCORRECT_REDEMPTION_PENALTY
+            #raise Exception("Code redeem failed!", result, result_details, receipt_info)
 
 
         if has_already:
+            attempt_count = attempt_count + STEAM_INCORRECT_REDEMPTION_PENALTY
             print("Already have game")
 
         # Key was redeemed.
